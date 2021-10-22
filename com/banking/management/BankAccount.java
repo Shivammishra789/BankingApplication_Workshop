fa8e2063dc012146fa21eac90597ebced38f7a8e
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-22 07:51:47 +0530

    welcome message

---
+++ com/banking/management/BankAccount.java
@@ -0,0 +1,8 @@
+package com.banking.management;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to banking acct management system!");
+	}
+}
