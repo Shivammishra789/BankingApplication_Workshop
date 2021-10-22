06fd2a187a20537684be9b336c2a8efef9e66494
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-22 12:02:48 +0530

    calculated monthly interest and added to savings balance

--- com/banking/management/BankAccount.java
+++ com/banking/management/BankAccount.java
@@ -5,25 +5,24 @@ public class BankAccount {
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to banking acct management system!");
-		
+		System.out.println();
 		SavingsAccount saver1 = new SavingsAccount(2000);
 		SavingsAccount saver2 = new SavingsAccount(3000);
 		
+		//calculating monthly interest 
 		saver1.calculateMonthlyInterest();
 		saver2.calculateMonthlyInterest();	
-		System.out.println(saver1);
-		System.out.println(saver2);
+		System.out.println("Monthly balance for saver1 is " + saver1 );
+		System.out.println("Monthly balance for saver2 is " + saver2 );
 		System.out.println();
-	
-		saver1.modifyInterestRate();
-		saver2.modifyInterestRate();
 		
+		//Modifying interest rate
+		SavingsAccount.modifyInterestRate(5);
+		
+		//calculating monthly interest for modified interest rate
 		saver1.calculateMonthlyInterest();
 		saver2.calculateMonthlyInterest();
-		System.out.println(saver1);
-		System.out.println(saver2);
-
-
-
+		System.out.println("Monthly balance for saver1 is " + saver1 );
+		System.out.println("Monthly balance for saver2 is " + saver2 );
 	}
 }
--- com/banking/management/SavingsAccount.java
+++ com/banking/management/SavingsAccount.java
@@ -3,48 +3,24 @@ package com.banking.management;
 public class SavingsAccount {
 
 	public static double annualInterestRate = 4;
-	private double savingsBalance;
+	private double savingsBalance;             
 
-	public SavingsAccount(double savingsBalance) {
+	public SavingsAccount(double savingsBalance) {		
 		super();
 		this.savingsBalance = savingsBalance;
 	}
-	
-	
-	public static double getAnnualInterestRate() {
-		return annualInterestRate;
+
+	public void calculateMonthlyInterest() {									//calculated monthly interest
+		double monthyInterest = savingsBalance * (annualInterestRate/ 12);
+		savingsBalance = savingsBalance + monthyInterest;	
 	}
 
-
-	public static void setAnnualInterestRate(double annualInterestRate) {
-		SavingsAccount.annualInterestRate = annualInterestRate;
+	public static void modifyInterestRate(double enteredAnnualInterestRate) {     //taking new interest rate
+		annualInterestRate = enteredAnnualInterestRate;
 	}
-
-
-	public double getSavingsBalance() {
-		return savingsBalance;
-	}
-
-
-	public void setSavingsBalance(double savingsBalance) {
-		this.savingsBalance = savingsBalance;
-	}
-
-
-	public void calculateMonthlyInterest() {
-		double monthyInterest = (savingsBalance * annualInterestRate) / 12;
-		savingsBalance = savingsBalance + monthyInterest;
-		
-	}
-	public static void modifyInterestRate() {
-	annualInterestRate = 6;
-	}
-	
-
 
 	@Override
 	public String toString() {
-		return "SavingsAccount [savingsBalance=" + savingsBalance + "]";
-	}
-	
+		return " "+savingsBalance;
+	}	
 }
